bf00c1c2feaceb22e6c90a726582fc46c592fe67
AbdelrahmanElkhradely <65429772+AbdelrahmanElkhradely@users.noreply.github.com>
2021-04-19 07:25:15 +0200

    modify data type of array

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import Load_DataSet
 import SplitDataSet
+import numpy as np
+
 if __name__ == '__main__':
     images,labels=Load_DataSet.get_dataset()
     test_Datamatrix, test_Labelmatrix, train_Datamatrix, train_Labelmatrix= SplitDataSet.split(images,labels)
-
+    test_Datamatrix = np.asarray(test_Datamatrix, dtype="int32")
+    test_Labelmatrix = np.asarray(test_Labelmatrix, dtype="int32")
+    train_Datamatrix = np.asarray(train_Datamatrix, dtype="int32")
+    train_Labelmatrix = np.asarray(train_Labelmatrix, dtype="int32")
+    
 
